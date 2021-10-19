4ef08fa282b2e7bded3f1f356706b0ee31da63a1
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-10-19 15:56:08 -0400

    SignOut functional

--- src/components/pages/auth/SignOut.js
+++ src/components/pages/auth/SignOut.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router";
+import { useFirebase } from "react-redux-firebase";
+import "./auth.scss";
 
 const SignOut = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const firebase = useFirebase();
+  const history = useHistory();
 
-export default SignOut
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const { value } = event.target;
+
+    switch (value) {
+      case "Sign Out":
+        firebase
+          .auth()
+          .signOut()
+          .then(() => {
+            history.push("/signin");
+          })
+          .catch((error) => {
+            window.alert(error.message);
+          });
+        break;
+      case "Cancel":
+        history.goBack();
+        break;
+      default:
+        console.log("Something went wrong.");
+        break;
+    }
+  };
+
+  return (
+    <div className="page-container">
+      <form>
+        <input type="button" value="Sign Out" onClick={handleClick} />
+        <br />
+        <input type="button" value="Cancel" onClick={handleClick} />
+      </form>
+    </div>
+  );
+};
+
+export default SignOut;
